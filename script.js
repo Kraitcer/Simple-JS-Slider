4977304ac944367e82d9f24b268f09b4ee085688
Gleb Kraitcer <kraitcer@gmail.com>
2022-11-11 09:35:15 +0300

    JS Slider

---
+++ script.js
@@ -0,0 +1,17 @@
+let offset = 0;
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".slider-next").addEventListener("click", function () {
+  offset = offset + 320;
+  if (offset > 1280) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + "px";
+});
+document.querySelector(".slider-prev").addEventListener("click", function () {
+  offset = offset - 320;
+  if (offset < 0) {
+    offset = 1280;
+  }
+  sliderLine.style.left = -offset + "px";
+});
